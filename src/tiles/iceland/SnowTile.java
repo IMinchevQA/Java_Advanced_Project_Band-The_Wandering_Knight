58e06ec6613aebd7a5ffd3f3987670fb54a0a570
AnaYaneva <ana_yaneva@abv.bg>
2017-03-07 23:27:51 +0200

    iceland added

---
+++ src/tiles/iceland/SnowTile.java
@@ -0,0 +1,13 @@
+package tiles.greenland;
+
+import gfx.Assets;
+import tiles.Tile;
+
+//GRASS TILE
+public class GrassTile extends Tile {
+
+    public GrassTile(int id) {
+        super(Assets.grass, id);
+    }
+
+}
---
+++ src/tiles/iceland/SnowyBush.java
@@ -0,0 +1,18 @@
+package tiles.greenland.decorations;
+
+import gfx.Assets;
+import tiles.Tile;
+
+//GRASS TILE
+public class Bush1 extends Tile {
+
+    public Bush1(int id) {
+        super(Assets.bush1, id);
+    }
+
+    @Override
+    public boolean isSolid(){
+        return true;
+    }
+
+}
